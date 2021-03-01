47490957d2bcbcc52ce1126bd22cb82684271218
007steve <stephen.plummer1.sp@gmail.com>
2021-03-01 14:21:33 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/users");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+//Cors
+const corsOpts = {
+  origin: [
+    "http://localhost:3000",
+    "https://music-player-frontend-007steve.vercel.app/",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  exposedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOpts));
+
+//Server
+const port = process.env.PORT || 5000;
+app.use(cookieParser());
+app.use(express.json());
+
+//Routes
+app.use("/api", userRouter);
+app.listen(port, () => console.log(`server is running great ${port}`));
+
+//Momgoose connection
+mongoose.connect(
+  process.env.ATLAS_URI,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    else {
+      return console.log(
+        "MongoDB database connection established successfully"
+      );
+    }
+  }
+);
+
